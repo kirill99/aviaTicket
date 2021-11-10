a66c9173edbb58e585af8ee68c500547e1e68cdd
Kirill Safonov <safonovkirill1995@gmail.com>
2021-11-10 18:37:55 +0600

    form init, api request

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,43 @@
 import "../css/style.css";
 import "./plugins";
 import locations from './store/location.store';
+import formUI from './views/form.view';
+import currencyUI from './views/currency.view'
 
 
-locations.init().then(res => {
-    console.log(res);
-    console.log(locations);
-    console.log(locations.getCitesByCountryCode("PE"));
-});
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+    const form = formUI.form;
+
+    // Events
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onFormSubmit();
+    })
+
+    // Handlers
+    async function initApp() {
+        await locations.init();
+        formUI.setAutoCompliteData(locations.shortCitiesList);
+    }
+
+    async function onFormSubmit() {
+        // 
+        const origin = locations.getCityCodeByKey(formUI.originValue);
+        const destination =locations.getCityCodeByKey(formUI.destinationValue);
+        const depart_date = formUI.departDateValue;
+        const return_date = formUI.returnDateValue;
+
+        const currency = currencyUI.currency.value;
+
+
+        // Code, Code, 2019-09, 2019-10
+        await locations.fetchTickets({
+            origin,
+            destination,
+            depart_date,
+            return_date,
+            currency,
+        })
+    }
+})
--- src/js/plugins/materialze.js
+++ src/js/plugins/materialze.js
@@ -1,5 +1,33 @@
 import 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css'
 
-var elems = document.querySelectorAll('select');
-M.FormSelect.init(elems);
+
+// Init select
+var select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+export function getSelectInstance(elem){
+    return M.FormSelect.getInstance(elem)
+}
+
+
+// Init autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete);
+
+export function getAutocompliteInstance(elem){
+    return M.Autocomplete.getInstance(elem)
+}
+
+
+
+// Init datepickers
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+    showClearBtn: true,
+    format: 'yyyy-mm',
+});
+
+export function getDatepickerInstance(elem){
+    return M.Datepicker.getInstance(elem)
+}
